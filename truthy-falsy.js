35fc23fbd363ef1bba6df42951b62ca8e7f78708
JosiasNetto <josiascaldasnetto08@gmail.com>
2025-01-02 23:48:29 -0300

    truthy and falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,24 @@
+//Truthy
+//Values that are translated to true when used in a boolean context
+//Exemples:
+if (true);
+if ({});
+if ([]);
+if (42);
+if ("foo");
+if (new Date());
+if (-42);
+if (3.14);
+if (-3.14);
+if (Infinity);
+if (-Infinity);
+
+//Falsy
+//Values that are translated to false when used in a boolean context
+//Exemples:
+if (false);
+if (null);
+if (undefined);
+if (0);
+if (-0);
+if ('');
